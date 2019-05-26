c48dcb5e10362d5baab67cb9c2de6974c4d88252
maaguilarca <maaguilarca@unal.edu.co>
2019-05-26 18:23:22 -0500

    aqui esta la ui mas fea del mundo

--- mobile_app/BankApp/app/src/main/java/com/example/bankapp/MainActivity.java
+++ mobile_app/BankApp/app/src/main/java/com/example/bankapp/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.bankapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextUsername = (EditText) findViewById(R.id.edituser);
+        mTextPassword = (EditText) findViewById(R.id.editpassword);
+        mButtonLogin = (Button) findViewById(R.id.buttonlogin);
     }
 }
